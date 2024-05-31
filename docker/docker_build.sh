c9b471d8c85a5dd3e7b78e761a29a37c28f6d6f4
Peter Hoenig <hpfreak1004@gmail.com>
2024-05-31 11:52:11 +0200

    fix install utility

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --rm -t bproc .
+docker build --rm -t blenderproc .
--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -6,5 +6,5 @@ docker run \
 --shm-size=8gb --env="DISPLAY" \
 --volume="/dev/bus/usb:/dev/bus/usb" \
 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
---volume="/home/peter/media/ssd2/blenderproc:/blenderproc" \
+--volume="/home/peter/media/ssd2/BlenderProc:/blenderproc" \
 --name=blenderproc_v0 blenderproc
