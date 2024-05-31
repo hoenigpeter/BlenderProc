a1b9b2a61a427c9547e515943a375476511bdfe4
Peter Hoenig <hpfreak1004@gmail.com>
2024-05-31 10:11:33 +0200

    update

---
+++ docker/docker_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run \
+--gpus all \
+-it \
+--shm-size=8gb --env="DISPLAY" \
+--volume="/dev/bus/usb:/dev/bus/usb" \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--volume="/home/peter/media/ssd2/blenderproc:/blenderproc" \
+--name=blenderproc_v0 blenderproc
