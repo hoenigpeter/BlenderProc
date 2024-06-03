21dc081106c6ed6bda4ebec4104819d307a65008
Peter Hoenig <hpfreak1004@gmail.com>
2024-06-03 11:18:17 +0200

    update

--- examples/datasets/bop_challenge/cc_texture_randomizer_2.py
+++ examples/datasets/bop_challenge/cc_texture_randomizer_2.py
@@ -14,6 +14,8 @@ parser.add_argument('--num_repeats', type=int, default=30, help='The number of t
 
 args = parser.parse_args()
 
+random.seed(42)
+
 # Load textures
 cc_textures = bproc.loader.load_ccmaterials(args.source_dir)
 
